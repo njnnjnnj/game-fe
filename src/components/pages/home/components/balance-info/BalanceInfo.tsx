1f1d1c9e2318da2a218cd680083ffdf3f3a4c64a
vhoreho <vpubuntu@gmail.com>
2025-02-27 22:29:56 +0300

    fixed data for sending when working clicker

--- src/components/pages/home/components/balance-info/BalanceInfo.tsx
+++ src/components/pages/home/components/balance-info/BalanceInfo.tsx
@@ -25,7 +25,7 @@ export const BalanceInfo: FunctionComponent<Props> = ({
     <div className="relative z-20 pt-6">
       <div className="mb-3 flex items-center gap-3">
         <StarSvg className="size-10" />
-        <span className="text-stroke-1 text-[40px] font-black leading-none text-white text-shadow">
+        <span className="text-stroke-1 text-[40px] font-black leading-none text-white text-shadow-sm">
           {formatNumber(balance)}
         </span>
       </div>
