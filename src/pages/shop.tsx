974b2baf55c450805d6a903ebf08bef34a19ac2b
maisonwork <frontend@maison-schweiz.com>
2025-03-14 23:27:38 +0300

    Updated shop ssr request

--- src/pages/shop.tsx
+++ src/pages/shop.tsx
@@ -1,9 +1,26 @@
 import React from "react";
 
-import { Shop } from "@/components/pages";
-import { getServerSidePropsWithLocale } from "@/utils/lib/translations";
+import { GetServerSideProps } from "next";
 
-export const getServerSideProps = getServerSidePropsWithLocale;
+import { parse } from "cookie";
+
+import { Shop } from "@/components/pages";
+
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  const { locale, req } = context;
+
+  const cookies = parse(req.headers.cookie || "");
+
+  const cookieLocale = cookies["NEXT_LOCALE"];
+
+  return {
+    props: {
+      messages: (
+        await import(`../../public/messages/${cookieLocale ?? locale}.json`)
+      ).default,
+    },
+  };
+};
 
 const ShopPage = () => <Shop />;
 
