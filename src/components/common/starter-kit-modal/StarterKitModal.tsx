76120c3cf79a688bf7559b23444891606253792f
maisonwork <frontend@maison-schweiz.com>
2025-03-19 14:25:57 +0300

    Added middleware for checking device

--- src/components/common/starter-kit-modal/StarterKitModal.tsx
+++ src/components/common/starter-kit-modal/StarterKitModal.tsx
@@ -41,7 +41,7 @@ export const StarterKitModal: FunctionComponent<Props> = ({
   const isEnabled = profile ? !profile.haveStarterPack : false;
 
   return (
-    <DrawerContent className="flex h-[70%] w-full flex-col items-center overflow-hidden rounded-t-3xl border-white/10 bg-blue-700 px-4 pb-8 pt-9 font-rubik shadow-[0_-8px_12px_0_rgba(5,22,37,0.6)]">
+    <DrawerContent className="z-50 flex h-[70%] w-full flex-col items-center overflow-hidden rounded-t-3xl border-white/10 bg-blue-700 px-4 pb-8 pt-9 font-rubik shadow-[0_-8px_12px_0_rgba(5,22,37,0.6)]">
       <DrawerClose asChild className="absolute right-4 top-4 z-10">
         <CloseIcon onClick={() => handleSelectionChanged()} />
       </DrawerClose>
---
+++ src/middleware.ts
@@ -0,0 +1,20 @@
+import { NextRequest, NextResponse } from 'next/server';
+
+import { ROUTES } from './constants/routes';
+
+export function middleware(req: NextRequest) {
+  const userAgent = req.headers.get('user-agent') || '';
+
+  const isMobile = /Android|webOS|iPhone|iPad|iPod|BlackBerry|Windows Phone/i.test(userAgent);
+
+  if (!isMobile) {
+    return NextResponse.redirect(new URL(ROUTES.FORBIDDEN, req.url));
+  }
+
+  return NextResponse.next();
+}
+
+export const config = {
+  matcher: ['/', '/((?!_next/static|_next/image|favicon.ico|forbidden).*)']
+};
+
