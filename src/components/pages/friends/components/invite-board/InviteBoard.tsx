10496c2ceb48f4fd4df50397d07b7f043494318e
maisonwork <frontend@maison-schweiz.com>
2025-03-20 21:41:34 +0300

    added alternative text

--- src/components/pages/friends/components/invite-board/InviteBoard.tsx
+++ src/components/pages/friends/components/invite-board/InviteBoard.tsx
@@ -61,7 +61,7 @@ export const InviteBoard: FunctionComponent<Props> = ({ referralsData }) => {
             onClick={() => {
               handleSelectionChanged();
               webApp?.openTelegramLink(
-                `https://t.me/share/url?url=${getLinkToApp(referralsData.link)}&text=%5Cn🎁 500,000 free tokens are already waiting for you in Majestic!%5Cn ⭐️ I invite you to the game`,
+                `https://t.me/share/url?url=${getLinkToApp(referralsData.link, false)}&text=%5Cn🎁 500,000 free tokens are already waiting for you in Majestic!%5Cn ⭐️ I invite you to the game`,
               );
             }}
           >
