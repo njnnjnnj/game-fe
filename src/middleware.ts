e68af8a05fa9006408da18e79a6987c90dd61d3e
maisonwork <frontend@maison-schweiz.com>
2025-03-20 19:52:18 +0300

    Fix link to referal route

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,20 +1,20 @@
-import { NextRequest, NextResponse } from 'next/server';
+// import { NextRequest, NextResponse } from 'next/server';
 
-import { ROUTES } from './constants/routes';
+// import { ROUTES } from './constants/routes';
 
-export function middleware(req: NextRequest) {
-  const userAgent = req.headers.get('user-agent') || '';
+// export function middleware(req: NextRequest) {
+//   const userAgent = req.headers.get('user-agent') || '';
 
-  const isMobile = /Android|webOS|iPhone|iPad|iPod|BlackBerry|Windows Phone/i.test(userAgent);
+//   const isMobile = /Android|webOS|iPhone|iPad|iPod|BlackBerry|Windows Phone/i.test(userAgent);
 
-  if (!isMobile) {
-    return NextResponse.redirect(new URL(ROUTES.FORBIDDEN, req.url));
-  }
+//   if (!isMobile) {
+//     return NextResponse.redirect(new URL(ROUTES.FORBIDDEN, req.url));
+//   }
 
-  return NextResponse.next();
-}
+//   return NextResponse.next();
+// }
 
-export const config = {
-  matcher: ['/', '/((?!_next/static|_next/image|favicon.ico|forbidden).*)']
-};
+// export const config = {
+//   matcher: ['/', '/((?!_next/static|_next/image|favicon.ico|forbidden).*)']
+// };
 
