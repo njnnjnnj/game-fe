2b68970c795f8d208871f743e0bebd6c7944fc86
vhoreho <vpubuntu@gmail.com>
2025-03-22 14:18:37 +0300

    Merged from develop

--- src/utils/number.ts
+++ src/utils/number.ts
@@ -1,8 +1,8 @@
 export const formatNumber = (num?: number | string): string => {
-  if (typeof num !== 'number' && !num) return "";
+  if (typeof num !== "number" && !num) return "";
   return num.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".");
 };
 
 export function getRandomZeroOrOne() {
-  return Math.floor(Math.random() * 2); // Generates either 0 or 1
+  return Math.floor(Math.random() * 2);
 }
