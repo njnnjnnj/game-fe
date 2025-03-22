3858a99c62949b61aa5dc5332c19dab65ce1aaf8
vhoreho <vpubuntu@gmail.com>
2025-03-22 14:02:46 +0300

    Added middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,11 +1,12 @@
-import { NextRequest, NextResponse } from 'next/server';
+import { NextRequest, NextResponse } from "next/server";
 
-import { ROUTES } from './constants/routes';
+import { ROUTES } from "./constants/routes";
 
 export function middleware(req: NextRequest) {
-  const userAgent = req.headers.get('user-agent') || '';
+  const userAgent = req.headers.get("user-agent") || "";
 
-  const isMobile = /Android|webOS|iPhone|iPad|iPod|BlackBerry|Windows Phone/i.test(userAgent);
+  const isMobile =
+    /Android|webOS|iPhone|iPad|iPod|BlackBerry|Windows Phone/i.test(userAgent);
 
   if (!isMobile) {
     return NextResponse.redirect(new URL(ROUTES.FORBIDDEN, req.url));
@@ -15,6 +16,5 @@ export function middleware(req: NextRequest) {
 }
 
 export const config = {
-  matcher: ['/', '/((?!_next/static|_next/image|favicon.ico|forbidden).*)']
+  matcher: ["/", "/((?!_next/static|_next/image|favicon.ico|forbidden).*)"],
 };
-
